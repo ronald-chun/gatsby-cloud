5f572c5575576100536b53c270dd4c2a1a4e4a84
Ronald Wong <ronaldchunyin@gmail.com>
2020-05-30 12:01:58 +0800

    Revert "Updates"
    This reverts commit 8c85f1efc4a8ab67621e3d9cbc051c3c59d81d24.

---
+++ gatsby-config.js
@@ -0,0 +1,19 @@
+require('dotenv').config()
+
+module.exports = {
+  pathPrefix: "/gatsby-cloud",
+  siteMetadata: {
+    title: `Creative Portfolio`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    `gatsby-transformer-remark`,
+    {
+      resolve: `gatsby-source-datocms`,
+      options: {
+        apiToken: process.env.DATO_API_TOKEN,
+      },
+    },
+  ],
+}
